39aad6b681c5616925729a1e2081b3249e60fcce
Joe Jean <joeclef@hotmail.com>
2017-06-23 23:59:00 +0400

    Added the db models

--- README.md
+++ README.md
@@ -6,6 +6,15 @@
 ## RUN THE PROJECT
 * From the project root run	`docker-compose up` and the site will be up and running on `http://localhost:8000/`
 
+## RUNNING A COMMAND
+* All commands should be run from within the container. There a two ways to do that:
+
+1) If the container is already up and running, just `exec` into it:
+`docker exec -it nyuadalumniwebsite_web_1 bash`, then you can run your command from the new shell. 
+
+2) If the container is not yet running you can use something like:
+`docker-compose run web python manage.py test` to run the tests from within the container.
+
 ## CONTRIBUTION GUIDELINES
 * Create a branch from the master and submit a pull request.
 
---
+++ api/models.py
@@ -0,0 +1,85 @@
+from django.db import models
+import json
+
+class City(models.Model):
+	country = models.CharField(max_length=60)
+	city = models.CharField(max_length=60)
+	region = models.CharField(max_length=60)
+	latitude = models.CharField(max_length=60)
+	longitude = models.CharField(max_length=60) 
+
+	def __str__(self):     
+		return "City %s " % self.city
+
+class Company(models.Model):
+	name = models.CharField(max_length=100)
+
+	def __str__(self):
+		return "Company %s" % self.name
+		 
+class School(models.Model):
+	name = models.CharField(max_length=100)
+
+	def __str__(self):
+		return "School %s" % self.name
+
+class Major(models.Model):
+	name = models.CharField(max_length=100)
+
+	def __str__(self):
+		return "Major %s" % self.name
+
+class PreProfessionalTrack(models.Model):
+	name = models.CharField(max_length=100)
+
+	def __str__(self):
+		return "Pre-professional Track %s" % self.name
+
+class StudyAbroadSite(models.Model):
+	name = models.CharField(max_length=100)
+
+	def __str__(self):
+		return "Study Abroad Site %s" % self.name
+
+
+class User(models.Model):
+	net_id = models.CharField(max_length=12)
+	first_name = models.CharField(max_length=50)
+	last_name = models.CharField(max_length=50)
+	graduation_year = models.CharField(max_length=4)
+	preferred_email = models.CharField(max_length=40) 
+	social_networks = models.CharField(max_length=400, null=True)
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
+	city = models.ForeignKey(
+		City, 
+		on_delete=models.SET_NULL,
+		null=True
+	)
+	company = models.ForeignKey(
+		Company,
+		on_delete=models.SET_NULL,
+		null=True
+	)
+	school = models.ForeignKey(
+		School,
+		on_delete=models.SET_NULL,
+		null=True
+	)
+	majors = models.ManyToManyField(Major)
+	study_abroads = models.ManyToManyField(StudyAbroadSite)
+	preprofessional_tracks = models.ManyToManyField(PreProfessionalTrack)
+
+	def get_social_networks_links(self):
+		try:
+			return json.loads(self.social_networks)
+		except Exception as e:
+			return {}
+
+	def __str__(self):              # __unicode__ on Python 2
+		return "User %s %s. Netid %s" % (self.first_name, self.last_name, self.net_id)
+ 
+
+
+
+	
